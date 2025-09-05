70dda7bccd9aaf76d1d22c46aefe53662a87a031
Yulong Ran <yulong.ran@ncino.com>
2025-09-05 14:20:40 -0400

    save

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Set base path for GitHub Pages
-  basePath: process.env.NODE_ENV === 'production' ? '/ncino-jotto' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/ncino-jotto/' : '',
 };
 
 export default nextConfig;
